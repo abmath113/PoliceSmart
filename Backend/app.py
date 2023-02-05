0b7721bbe56b370c5075406683842f7204f520df
Arnav Singhal <87948031+Arnav047@users.noreply.github.com>
2023-02-05 10:21:09 +0530

    Add files via upload

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,56 @@
 from warnings import catch_warnings
+import pickle
+import numpy as np
 # from pymongo import MongoClient
+import pyrebase
+
 import random
 import pandas as pd
 from flask import Flask, render_template,request,redirect
 
 app = Flask(__name__)
+
+
+firebaseConfig={"apiKey": "AIzaSyBVO6-S0Qkg_uy-T-LvpcGYqPpbaTu7zlc",
+  "authDomain": "fir-policedata.firebaseapp.com",
+  "databaseURL": "https://fir-policedata-default-rtdb.firebaseio.com",
+  "projectId": "fir-policedata",
+  "storageBucket": "fir-policedata.appspot.com",
+  "messagingSenderId": "244873735032",
+  "appId": "1:244873735032:web:f469112a967e44a6a4b68d",
+  "measurementId": "G-530QMD84LL"}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+db=firebase.database()
+
+
+
+
+model = pickle.load(open("model_rank.pkl","rb"))
+@app.route('/')
+def hello_world(): 
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method == 'GET':
+        return render_template('predict.html',prediction = 'NULL')
+    else:
+        float_features = [float(x) for x in request.form.values()]
+        features = [np.array(float_features)]
+        prediction = model.predict(features)
+
+        if prediction[0] == 0:
+            rank = 'ASP'
+        elif prediction[0] == 1:
+            rank = 'DSP'    
+        elif prediction[0] == 2:
+            rank = 'Inspector'
+      
+
+        return render_template('predict.html',prediction = rank)
+
+
 
 @app.route('/CitizenSignUp', methods=['GET', 'POST'])
 def CitizenSignUp():
@@ -15,6 +61,9 @@ def CitizenSignUp():
         email = request.form.get("email")
         aadhar = request.form.get("aadhar")
         password = request.form.get("password")
+        data={'first':first,'last':last,'mobile':mobile,'email':email,'aadhar':aadhar,'password':password}
+        db.push(data)
+
 
         return redirect('/CitizenLogin')
 
@@ -26,6 +75,8 @@ def CitizenLogin():
     if request.method == "POST":
         password = request.form.get("password")
         aadhar = request.form.get("aadhar")
+        data={'password':password,'aadhar':aadhar}
+        db.push(data)
         return redirect('/complains')
 
     return render_template('login.html')
@@ -40,6 +91,8 @@ def PoliceSignUp():
         email = request.form.get("email")
         aadhar = request.form.get("aadhar")
         password = request.form.get("password")
+        data={'first':first,'last':last,'mobile':mobile,'email':email,'aadhar':aadhar,'password':password}
+        db.push(data)
 
         return redirect('/policeLogin')
 
@@ -51,6 +104,8 @@ def policeLogin():
     if request.method == "POST":
         password = request.form.get("password")
         police = request.form.get("Police")
+        data={'password':password,'police':police}
+        db.push(data)
         return redirect('/complains')
 
     return render_template('login_police.html')
@@ -66,10 +121,13 @@ def complain():
         crimetype = request.form.get("crimetype")
         description = request.form.get("describe")
         proof = request.form.get("proof")
+        data={'name':name,'aadhar':aadhar,'date':date,'victimname':victimname,'place':place,'crimetype':crimetype,'description':description,'proof':proof}
+        db.push(data)
 
         return render_template('Complain.html',message='done')
 
     return render_template('Complain.html')
+     
      
 
 if __name__ == '__main__':
